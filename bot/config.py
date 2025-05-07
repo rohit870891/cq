918d3e84dee75149c31875b1e134580e56eb7982
rohit870891 <koyeb77user@gmail.com>
2025-05-08 00:47:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,8 +4,8 @@ try:
     APP_ID = config("APP_ID", default=20718334, cast=int)
     API_HASH = config("API_HASH", default="4e81464b29d79c58d0ad8a0c55ece4a5")
     BOT_TOKEN = config("BOT_TOKEN", default="8065030679:AAE-Vvvejh6maceHLBamYQPOYIyC-sMUM2w")
-    DEV = config("DEV", default=5585016974, cast=int)
-    OWNER = config("OWNER", default="5585016974")
+    DEV = config("DEV", default=7328629001, cast=int)
+    OWNER = config("OWNER", default="7328629001")
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
